02dbac8469e42f08c8e5035e9b7706b67ca75a2d
Carmen Alvarez <carmen@rmen.ca>
2025-04-30 01:46:11 +0200

    Day 1 cleanup.
    * Prefix constants with C- an dput into working-storage.
    * Simplify the difference calculation by removing the temporary difference "row" variable.

--- 01/day01.cob
+++ 01/day01.cob
@@ -19,11 +19,13 @@
        01  F-DATA-RECORD             PIC X(100).
 
        WORKING-STORAGE SECTION.
-       01 WS-DELIMITER               PIC X(3) VALUE "   ".
-       LOCAL-STORAGE SECTION.
+       01 C-DELIMITER                CONSTANT "   ".
        01 C-MAX-FILE-LENGTH          CONSTANT 1000.
+
+       LOCAL-STORAGE SECTION.
        01 LS-FILE-PATH               PIC X(100).
        01 LS-FILE-LENGTH             PIC 9(4) VALUE 0.
+
        01 LS-TABLE-INDEX             PIC 9(4) VALUE 1.
        01 LS-DATA-TABLE-1 OCCURS 1 TO C-MAX-FILE-LENGTH TIMES
            DEPENDING ON LS-FILE-LENGTH.
@@ -32,9 +34,9 @@
            DEPENDING ON LS-FILE-LENGTH
            INDEXED BY LS-SEARCH-INDEX.
            05 LS-DATA-ITEM-2         PIC 9(5) COMP.
-       01 LS-DIFFERENCE-ROW          PIC 9(5).
-       01 LS-DIFFERENCE-ACC          PIC 9(10) VALUE 0.
-       01 LS-SIMILARITY-ACC          PIC 9(10) VALUE 0.
+
+       01 LS-DIFFERENCE              PIC 9(10) VALUE 0.
+       01 LS-SIMILARITY              PIC 9(10) VALUE 0.
        PROCEDURE DIVISION.
 
       *> Read the file path from the command line arguments.
@@ -48,7 +50,7 @@
                    EXIT PERFORM
                NOT AT END
                    UNSTRING F-DATA-RECORD
-                       DELIMITED BY WS-DELIMITER
+                       DELIMITED BY C-DELIMITER
                        INTO LS-DATA-ITEM-1(LS-TABLE-INDEX)
                             LS-DATA-ITEM-2(LS-TABLE-INDEX)
                    END-UNSTRING
@@ -67,11 +69,11 @@
            UNTIL LS-TABLE-INDEX > LS-FILE-LENGTH
       *> Part 1: Calculate the absolute difference for each pair of
       *> items from the two tables, and display the sum.
-               COMPUTE LS-DIFFERENCE-ROW = FUNCTION ABS(
-                   LS-DATA-ITEM-1(LS-TABLE-INDEX) -
-                   LS-DATA-ITEM-2(LS-TABLE-INDEX))
-               COMPUTE LS-DIFFERENCE-ACC = LS-DIFFERENCE-ACC
-                   + LS-DIFFERENCE-ROW
+               COMPUTE LS-DIFFERENCE = LS-DIFFERENCE +
+                   FUNCTION ABS(
+                       LS-DATA-ITEM-1(LS-TABLE-INDEX) -
+                       LS-DATA-ITEM-2(LS-TABLE-INDEX)
+                   )
       *> Part 2: Calculate the number of times the item from the first
       *> table appears in the second table.
       *> The sum of these calculations is the similarity.
@@ -79,10 +81,10 @@
                    UNTIL LS-SEARCH-INDEX > LS-FILE-LENGTH
                        IF LS-DATA-ITEM-1(LS-TABLE-INDEX) =
                            LS-DATA-ITEM-2(LS-SEARCH-INDEX)
-                           COMPUTE LS-SIMILARITY-ACC = LS-SIMILARITY-ACC
+                           COMPUTE LS-SIMILARITY = LS-SIMILARITY
                                + LS-DATA-ITEM-1(LS-TABLE-INDEX)
                END-PERFORM
        END-PERFORM
-       DISPLAY LS-DIFFERENCE-ACC
-       DISPLAY LS-SIMILARITY-ACC
+       DISPLAY LS-DIFFERENCE
+       DISPLAY LS-SIMILARITY
        .
